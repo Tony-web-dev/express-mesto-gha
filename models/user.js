3a80371f411fb696b3e88300b0b7eca7d7d4a588
Antonina Timush <antoninatimush@yandex.ru>
2023-08-18 21:09:02 +0300

    fix autotest #2

--- models/user.js
+++ models/user.js
@@ -44,7 +44,8 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email }).select('+password')
+  return this.findOne({ email })
+    .select('+password')
     .then((user) => {
       if (!user) {
         throw new UnauthorizedError('Указаны неправильные почта или пароль');
